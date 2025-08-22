eb1c3853594b4ac4c552d1d31eb0d5461549cc9b
kuroshiro1902 <emsinos1902@gmail.com>
2025-08-22 08:18:07 +0700

    Java variable

---
+++ src/main/java/org/example/variable/VariableExample.java
@@ -0,0 +1,11 @@
+package org.example.variable;
+
+public class VariableExample {
+    int num = 10;
+    final int thisIsAConstant = 100;
+    int x, y, z;
+
+    public VariableExample(){
+        this.x = this.y = this.z = 50;
+    }
+}
