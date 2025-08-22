d17b24245c94f29e4f60579ab70b7a6b4a69229d
kuroshiro1902 <emsinos1902@gmail.com>
2025-08-22 08:44:47 +0700

    Java data type

---
+++ src/main/java/org/example/datatype/DataTypeExample.java
@@ -0,0 +1,13 @@
+package org.example.datatype;
+
+public class DataTypeExample {
+    byte byteVar = 127;
+    short shortVar = 32767;
+    int intVar = 1;
+    long longVar = 12334;
+    float floatVar = 10.0f;
+    double doubleVar = 10.123d;
+    char charVar = 'D';
+    boolean boolVar = true;
+    String stringVar = "Test";
+}
